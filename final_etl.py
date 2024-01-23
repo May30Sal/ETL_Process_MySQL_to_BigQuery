c0847f91f442056d5d5b5199cc898654780f6acd
Mayara Saldanha <maysal20@gmail.com>
2024-01-22 19:22:28 -0700

    Update final_etl.py

--- final_etl.py
+++ final_etl.py
@@ -13,7 +13,7 @@ table = 'stock_status'
 table_id = f'{project}.{dataset}.{table}'
 
 # Establishing data connection with MySQL
-conn = mysql.connector.connect(read_default_file='C:/Users/MAYSA/.my.cnf') 
+conn = mysql.connector.connect(read_default_file='C:/Users/USER/.my.cnf') 
 
 # Extracting data from MySQL and Transforming it
 query = '''
